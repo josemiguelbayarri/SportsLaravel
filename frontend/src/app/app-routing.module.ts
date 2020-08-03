581e70b0c1c0adadfb1223c1a25a3c8ba324a161
josebayarri <josemiguelbayarri@gmail.com>
2020-08-03 19:31:53 +0200

    footer modificated

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CitasComponent } from './components/citas/citas.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"citas", component: CitasComponent},
+  {path:"", component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
